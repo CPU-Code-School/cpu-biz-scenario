181c27442dc2e4b7ea1699462764ebc4312c3f87
Leah Bineth <83653442+LeahBineth@users.noreply.github.com>
2022-04-28 19:01:53 +0300

    spec

---
+++ Real Estate.sql
@@ -0,0 +1,59 @@
+/*
+I run a residential Real Estate business based in Lakewood since 2009. We don't actually own the houses. We are just the Realtors.
+Until now we only bought and sold in Lakewood. We didn't even enter the town into our sytem because it was a given. 
+Now, with the rapid growth of the community, there isn't much to buy and sell in Lakewood anymore.
+We began to expand to buy and sell in Jackson, Toms River, Howell, Brick , and Manchester. We now need to record what town each house is located 
+in. When we tried to amend our program we noticed many weaknesses in our current system. We decided we are best off starting from scratch.
+
+We record the address of the house, the town the house is in, the type of house (bi-level, colonial, ranch, split-level, duplex, townhouse
+or vacant land), and the sqare-footage of the house. We also want to record amount of Bathrooms and Bedrooms 
+We don't do any renting, we do sometimes sell individual apartments.
+We need to record who owns the house and which one of our Realtors is working on this house. We also need to record who our client is.  
+Sometimes houses are owned by entities so we need to know who is the individual we are in contact with.  We also want to record on what date 
+was the house put on the market and if it was sold to who and on what date. I would also like to record the asking price and the price the 
+house was actually sold for.
+
+I also want to record, if the house is not sold, is it in contract yet? if so, then the only column blank is the close date.
+
+Q: Do you enter the houses into your system before they are officially on the market?
+A: Usually when houses are entered into the system, that is when they are on the market. Sometimes when we are overworked (at the end of the 
+year) houses are bought and sold before they are entered...but it is usually around the date it was entered and it only happens to houses 
+that were sold on the same day or the next day. Therefore, we should have another column for when the house was put into the system and
+default the date a house was put on the market to be the date it was sold. It is better that the date it was put on the market should be
+a day off rather than blank. It should allow to enter a date that past but not a date in the future
+    We don't need to record time. Just a date is enough.
+
+Q: Are houses ever sold before they are put on market?
+A: No, sometimes they are sold the same day
+
+Q: Do you have any other types of houses besides for what was listed above? 
+A: No, we do sell apartments once in a while.
+
+Q: Do you deal with houses out of the towns listed above? 
+A: Not as of yet, but with  the rapid growth of the area, you never know.
+
+Q: What's the least and most you would sell a house for?
+A: We don't sell houses for less than $100,000. Even when we sell apartments, we only sell if we could sell for $100,000. So far we never sold 
+for more than 1.5 Million. With today's inflation and rising Real Estate market, I want the system to allow up to 9.9 million
+
+Q: Do you ever sell for less than the asking price?
+A: We don't
+
+Q: Do you want to record the Sqare footage of the house or the plot of land? 
+A: Both, put it in 2 seperate columns
+
+Reports:
+
+1) I want a report of all the houses sorted by the block and then by the town/city
+2) I want a report of all the houses sorted by realtor
+3) a report of how long it took for each house to sell
+4) the price difference from the asking price to the sold price
+
+Sample data:
+5 Lynn Drive, Toms River, Colonial, 4 Bdrms, 2.5 baths, house-3000, lot-42000 , Owner - Lynn Drive, LLC , Client- Yaakov Fishman, Realtor - Rivka Harnik, 1/12/2021, 2/22/2021, asking-450k, sold 475k, buyer-Rachel Gestetner
+8 London Drive, Lakewood, Ranch, 3 bdrms, 2 baths, house-2000, lot - 4089, owner-Shaindy Braun, Client-Shaindy Braun, realtor, Raizy Berger, 4/5/2009, 7/10/2010, asking 200k, sold 200k, buyer-Elazar and Faigy Adler
+423 2nd Street, Lakewood, Colonial, 9 bdrms, 5.5 baths, house 3500, lot-4200, owner- L3C Jackson, LLC, client-Mark Farkas, Rivka Harnik, 1/6/2015, 6/9/2015, asking 360k, sold 370k, buyer Yossi Handler and Rivky Handler
+176 Hadassah Lane, Lakewood, duplex, 5 bdrms, 2.5 baths, house-2550, lot-3049.2 , Greenview Equities, LLC, shlomo press, Moshe Celnik, 5/3/2021, not sold, asking 549k, selling 600k, buyer Shea Speigel, under contract
+1141 Central Avenue, Lakewood, ranch, 3 bdrms, 1 bath, house-855, lot-5000, Sorah Hager, Yitzchok Tendler, Moshe Celnik, 1/2/2022, not sold, asking 300k, not in contract
+
+*/
