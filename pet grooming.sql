bd31a63ad32677572cbd694223ab94a5f02d825c
leahzilber <leahzilber1@gmail.com>
2022-11-09 21:37:13 +0000

    spec

---
+++ pet grooming.sql
@@ -0,0 +1,44 @@
+/* 
+Hi,
+I run a mobile pet grooming business that began in 2019. Our business has recently grown considerably as people allow us back into their homes. With this increase, we need 
+an organized method of keeping track of our customers (and their pets). We need to record the pet owner's name, adress, what kind of pet they have, the name of their pet, 
+price per grooming, frequency of service, and the date we picked them up.
+
+Questions:
+Q)What are your options for frequency of service?
+A)We only offer weekly and biweekly service.
+
+Q)Do you need to keep track of previous customers?
+A)Yes, at present, we have had no customers drop us, but we should have a column available for when we need it.
+
+Q)Do you offer your services for all animals?
+A)No, we only offer grooming for dogs, cats, rabbits, and guinea pigs.
+
+Q)Can one customer have multiple pets?
+A)Yes, we record each pet seperately, and just repeat the owner's information for each pet.
+
+Reports:
+1)We need a list of how many of each pet we currently have.
+2)we need to see how many customers have multiple pets.
+3)We need a list of the number of each kind of animal we have, for advertising and hiring purposes
+4)We need to know the average amount we make per animal type 
+
+Sample Data:
+Bry-Ann Yates, 326 34th St. S, rabbit, Longears, $30, weekly, August 21 2019
+Meg Ross, 1719 Beach Dr SE, dog, Trooper, $55, biweekly, January 19 2020
+Brayanna Mille, 2255 22 Ave N, rabbit, Hunny Bunny $40, biweekly, November 5 2019
+Brayanna Mille, 2255 22 Ave N, rabbit, Hazel, $40, biweekly, November 5 2019
+Marianne Griffin, 312 Sand Pine Ln, dog, Mr. Stich, $60, biweekly, June 20 2021
+Mike Smith, 145 Menhaden St, guinea pig, Pippin, $30, biweekly, April 30 2022
+Bethany Singer, 1818 Bay St, cat, Dingus, $40, biweekly, June 7 2022
+Bobbi Welker, 324 Wilcox St, dog, Moose, $45, weekly, March 14 2021
+Bobbi Welker, 324 Wilcox St, dog, Piper, $60, weekly, March 14 2021
+Bobbi Welker, 324 Wilcox St, dog, Kipper, $65, weekly, March 14 2021
+Mark Doppler, 5329 53rd St, guinea pig, Ginger, $35, biweekly, October 29 2019
+Tara Hamid, 210 Sunrise Dr., rabbit, Holly, S50, biweekly, December 12 2021
+Leni Baker, 3210 Gandy Blvd., cat, Pussy Willow, $55, weekly, September 24 2019
+Leni Baker, 3210 Gandy Blvd., cat, Kitty Cat, $60, weekly, September 24 2019
+Heather Rieder, 937 MLK St., rabbit, Hopper, $45, weekly, February 2 2022
+Lee Kleshinski, 4903 49th Ave, dog, Phillip, $60, weekly, July 8 2021
+Tracy Price, 9027 Juniper St, rabbit, Dopey, $55, biweekly, September 30 2019
+Tracy Price, 9027 Juniper St, guinea pig, Spicy, $40, biweekly, September 30 2019
