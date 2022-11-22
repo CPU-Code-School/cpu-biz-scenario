5ef7d16f16c8d17b0e856687f17c7016aec4cdca
DassyZahn <dassysilkin@gmail.com>
2022-11-22 17:54:29 +0200

    #17

--- work/1 Table.sql
+++ work/1 Table.sql
@@ -5,13 +5,14 @@ go
 create table dbo.Costumes(
 CostumeId int not null identity primary key,
 CustomerName varchar (50) not null constraint ck_costumes_customername_cannot_be_blank check(customername <> ''),
-CostumesBought varchar (20) not null constraint ck_costumes_CostumesBought_cannot_be_blank check (CostumesBought <> ''),
-Size varchar (2) not null constraint ck_costumes_size_cannot_be_blank check (Size <> ''),
-AmountBought char (1) constraint ck_costumes_AmountBought_cannot_be_blank check (AmountBought <> ''),
+CostumesDesc varchar (20) not null constraint ck_costumes_CostumesBought_cannot_be_blank check (CostumesDesc <> ''),
+Size varchar (2) not null constraint ck_costumes_size_can_only_be_xs_till_xl check (Size in ('xs','s','m','l','xl')),
+AmountBought int constraint ck_costumes_AmountBought_cannot_be_lee_than_0 check (AmountBought > 0),
 CostPricePerCostume as case size when 'xs' then 15 when 's' then 17 when 'm' then 20 when 'l' then 22 when 'xl' then 25 end,
 SoldPricePerCostume decimal (4,2)constraint ck_costumes_SoldPricePerCostume_must_be_greater_or_equal_to_15 check (SoldPricePerCostume >= 15),
-DateBought date not null,
+DateBought date not null constraint ck_costumes_datebought_must_be_after_jan_01_2022 check (datebought > '2020-01-01'),
 DateSold date null, 
+Paidfull bit not null,  
 CONSTRAINT ck_Datebought_must_be_before_Datesold check(Datebought < Datesold),
 )
 
--- work/3 reports.sql
+++ work/3 reports.sql
@@ -1,20 +1,20 @@
-
-
+use CostumeDB
+go
 --I am going to need the following reports:
 -- 1. I need to know which costume is the most popular.     `|
-select CostumesBought, totalamount = count(AmountBought)
+select top(1) c.CostumesDesc, totalamount = count(c.AmountBought)
 from costumes c 
-group by CostumesBought
+group by c.CostumesDesc
 order by totalamount desc 
 
 -- 2. I need to know which size is the most polpular.
-select size, totalamount = count(AmountBought)
+select top(1) c.size, totalamount = count(c.AmountBought)
 from costumes c 
-group by size
+group by c.size
 order by totalamount desc 
 
 -- 3. I need you to show me all of my customers in the following format: name: amountbought - costume customer bought (how much they paid)
-select concat(c.CustomerName, ': ', c.AmountBought, ' - ', c.CostumesBought, '(', (c.SoldPricePerCostume * c.amountbought), ')')
+select CustomerDetails = concat(c.CustomerName, ': ', c.AmountBought, ' - ', c.CostumesDesc, '(', (c.SoldPricePerCostume * c.amountbought), ')')
 from Costumes c
 
 -- 4. I need to know the profit each sale.
