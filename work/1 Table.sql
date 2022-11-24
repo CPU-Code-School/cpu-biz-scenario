d8ba78de067cbbe641cb618799fcecc93ead31c6
DassyZahn <dassysilkin@gmail.com>
2022-11-24 18:14:00 +0200

    #21

--- work/1 Table.sql
+++ work/1 Table.sql
@@ -5,7 +5,7 @@ go
 create table dbo.Costumes(
 CostumeId int not null identity primary key,
 CustomerName varchar (50) not null constraint ck_costumes_customername_cannot_be_blank check(customername <> ''),
-CostumesDesc varchar (20) not null constraint ck_costumes_CostumesBought_cannot_be_blank check (CostumesDesc <> ''),
+CostumesDesc varchar (20) not null constraint ck_costumes_CostumesDesc_cannot_be_blank check (CostumesDesc <> ''),
 Size varchar (2) not null constraint ck_costumes_size_can_only_be_xs_till_xl check (Size in ('xs','s','m','l','xl')),
 AmountBought int constraint ck_costumes_AmountBought_cannot_be_lee_than_0 check (AmountBought > 0),
 CostPricePerCostume as case size when 'xs' then 15 when 's' then 17 when 'm' then 20 when 'l' then 22 when 'xl' then 25 end,
