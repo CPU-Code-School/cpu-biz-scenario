a29fa064eb5e5df1499ea0fe3ad60a74337859fd
avst96 <avst96@gmail.com>
2023-02-14 13:52:01 -0600

    Requested Changes

--- FlyAirDB/1 Table.sql
+++ FlyAirDB/1 Table.sql
@@ -12,15 +12,15 @@ create table dbo.FlightInfo (
     ArrivalAirport char(3) not null constraint ck_FlightInfo_ArrivalAirport_cannot_contain_spaces check(ArrivalAirport not like '% %'), 
     ArrivingCountry varchar(25) not null constraint ck_FlightInfo_ArrivingCountry_cannot_be_blank check(ArrivingCountry > ''), 
     DepartingDateTime datetime not null constraint ck_FlightInfo_DepartingDateTime_cannot_be_more_then_a_year_from_today
-        check(DepartingDateTime < getdate() + 365), --It seems to work fine even though I didn't specify that I am adding days. 
+        check(datediff(day,getdate(),DepartingDateTime) < 365),  
         --Should probably add constraint that it shouldnt be in the pass, but it would conflict with sample data.
     ArrivingDateTime datetime not null, 
     FirstName varchar(30) not null constraint ck_FlightInfo_FirstName_cannot_be_blank check(FirstName > ''), 
     LastName varchar(30) not null constraint ck_FlightInfo_LastName_cannot_be_blank check(LastName > ''), 
-    DOB date not null constraint ck_FlightInfo_DOB_cannot_be_in_the_future check(DOB <= getdate()), 
+    DOB date not null, 
     PassAddress varchar(150) not null constraint ck_FlightInfo_Address_cannot_be_blank check(PassAddress > ''), 
     CheckedIn bit not null default 0, 
-    PassportNum int null constraint ck_FlightInfo_PassportNum_must_be_9_num_and_not_start_with_0 check(PassportNum between 100000000 and 999999999),    
+    PassportNum char(9) null constraint ck_FlightInfo_PassportNum_must_be_9_num_and_not_start_with_0 check(PassportNum like '[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),    
     IssueDate date null,
     ExpiryDate date null, 
     Nationality varchar(30) null constraint ck_FlightInfo_Nationality_cannot_be_blank check(Nationality > ''),
@@ -28,12 +28,11 @@ create table dbo.FlightInfo (
         check((datediff(day,getdate(),DepartingDateTime) > 30 and CheckedIn = 0) or datediff(day,getdate(),DepartingDateTime) < 30),
     constraint ck_FlightInfo_ArrivalDateTime_must_be_after_depaurturedatetime_and_less_then_3_days_later
         check(ArrivingDateTime between DepartingDateTime and DepartingDateTime + 3), 
-    constraint ck_FlightInfo_DOB_must_be_between_16_and_90_by_departure_date check(datediff(day,DOB,DepartingDateTime) between 5840 and 32850),
-    ---There doesnt seem to be a completely accurate way to do it, as even using 365 days is not completly accurate, and definitly not years which only show how many times the year has changed
-    constraint ck_FlightInfo_IssueDate_not_valid check(IssueDate < ExpiryDate and IssueDate >= DOB),
+    constraint ck_FlightInfo_DOB_must_be_between_16_and_90_by_departure_date check(datediff(year,DOB,DepartingDateTime) between 16 and 90),
+        constraint ck_FlightInfo_IssueDate_not_valid check(IssueDate < ExpiryDate and IssueDate >= DOB),
         constraint ck_FlightInfo_ExpiryDate_not_valid_for_travel 
-        check(((DepartureCountry = ArrivingCountry or Nationality = ArrivingCountry or datediff(year,DOB,IssueDate) < 16) and ExpiryDate >= DepartingDateTime) 
-            or datediff(day,DepartingDateTime,ExpiryDate) > 182),
+        check(((DepartureCountry = ArrivingCountry or Nationality = ArrivingCountry or datediff(year,DOB,IssueDate) < 16) and ExpiryDate >= ArrivingDateTime) 
+            or datediff(day,ArrivingDateTime,ExpiryDate) > 182),
     constraint u_PassportNum_can_only_have_one_seat_on_flight unique(PassportNum,FlightNum,DepartingDatetime,FirstName,LastName),
     constraint ck_FlightInfo_PassportNum_and_all_passport_info_must_be_null_or_not_null 
         check((PassportNum is null and IssueDate is null and ExpiryDate is null and Nationality is null) 
--- FlyAirDB/3 Reports.sql
+++ FlyAirDB/3 Reports.sql
@@ -10,7 +10,8 @@ group by f.FlightNum, f.DepartureAirport, f.DepartingDateTime, f.ArrivingCountry
 
 select *
 from FlightInfo f 
-where CheckedIn = 0 and datediff(day, getdate(), DepartingDateTime) between 0 and 7 -- I don't want to include flights already departed
+where CheckedIn = 0 
+    and datediff(day, getdate(), DepartingDateTime) between 0 and 7 -- I don't want to include flights already departed
 
 --3) How many flights are departing per day, and num of passengers we have on those flights.
 
@@ -25,20 +26,19 @@ group by f.FlightNum, f.DepartingDateTime
 
 --4) How many flights are departing per destination, and num of passengers we have on those flights, to know what route is attracts most people.
 
-select count(*) as FlightsPerDestination, f.ArrivingCountry
+select count(*) as PassengersPerDay, f.ArrivingCountry
 from flightinfo f 
 group by f.ArrivingCountry
 order by count(*) desc
 
-select count(*) as PassengersPerDestination, f.ArrivingCountry
-from FlightInfo f 
-group by f.ArrivingCountry, f.PassportNum
+
 
 --5) How many people booked but didn't actually travel in the end.
 
 select count(*) as BookedAndMissedFlight
 from FlightInfo f 
-where f.DepartingDateTime < getdate() and f.CheckedIn = 0 --Doesn't account for people that checked in and didn't fly
+where f.DepartingDateTime < getdate() 
+    and f.CheckedIn = 0 --Doesn't account for people that checked in and didn't fly
 
 --6) How many flights does each person have (to know for frequent flyer status), as: last name, first name, number of flights.
 
