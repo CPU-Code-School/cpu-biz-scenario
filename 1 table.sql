93e035e64ddd42bc089f09c0b34f69604d374f84
YFisch <121062368+YFisch@users.noreply.github.com>
2023-04-24 15:20:37 -0400

    Fixed Mistake

--- 1 table.sql
+++ 1 table.sql
@@ -15,6 +15,5 @@ create table dbo.MusicRental(
     DateReturned date constraint ck_MusicRental_date_returned_cannot_be_a_future_date check(DateReturned < getdate()),
     DateInserted datetime not null default GETDATE(),
     constraint ck_MusicRental_DateReturned_must_be_after_dateRented check(DateReturned > DateRented),
-    constraint ck_MusicRental_TotalProfit_and_DateReturned_must_be_either_both_null_or_both_supplied check((TotalProfit is null and DateReturned is null)or(TotalProfit is not null and DateReturned is not null )),
     constraint ck_MusicRental_between_DateReturned_and_DateRented_there_cant_be_more_than_12_months_and_less_than_1_month check(DATEDIFF(month, dateRented, DateReturned) >=1 and DATEDIFF(month, dateRented, DateReturned) <12)
 )
