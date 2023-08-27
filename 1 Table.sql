3d284e1b71e3ef2b6c3d62b2559bc6ec976a09a2
fbernstein <fcb613@gmail.com>
2023-08-27 11:52:50 -0400

    #22 Corrections

--- 1 Table.sql
+++ 1 Table.sql
@@ -1,17 +1,17 @@
 Use PetDB
 GO
 
-Drop table if exists Pet 
+Drop table if exists Customer 
 GO
-create table dbo.Pet(
-    PetId int identity primary key,
-    OwnerName varchar(50) not null constraint ck_Pet_OwnerName_cannot_be_blank check(Ownername > ''),
-    OwnerAddress Varchar(50) not null constraint ck_Pet_OwnerAddress_cannot_be_blank check(OwnerAddress > ''),
-    PetType varchar(15) not null constraint ck_Pet_PetType_must_be_species_that_we_service check(PetType in ('dog','cat','rabbit','guinea pig')),
-    PetName varchar(50) not null constraint ck_Pet_PetName_cannot_be_blank check(PetName > ''),
-    Price decimal (5,2) not null constraint ck_Pet_Price_must_be_greater_than_zero check(Price > 0),
-    Frequency varchar(8) not null constraint ck_Pet_Frequency_must_be_weekly_or_biweekly check(Frequency in('weekly','biweekly')),
-    FirstServiceDate DATE not null constraint ck_Pet_FirstServiceDate_must_be_after_business_opened_2019 check (year(FirstServiceDate) > 2018),
+create table dbo.Customer(
+    CustomerId int identity primary key,
+    OwnerName varchar(50) not null constraint ck_Customer_OwnerName_cannot_be_blank check(Ownername > ''),
+    OwnerAddress Varchar(50) not null constraint ck_Customer_OwnerAddress_cannot_be_blank check(OwnerAddress > ''),
+    PetType varchar(15) not null constraint ck_Customer_PetType_must_be_species_that_we_service check(PetType in ('dog','cat','rabbit','guinea pig')),
+    PetName varchar(50) not null constraint ck_Customer_PetName_cannot_be_blank check(PetName > ''),
+    Price decimal (5,2) not null constraint ck_Customer_Price_must_be_greater_than_zero check(Price > 0),
+    Frequency varchar(8) not null constraint ck_Customer_Frequency_must_be_weekly_or_biweekly check(Frequency in('weekly','biweekly')),
+    FirstServiceDate DATE not null constraint ck_Customer_FirstServiceDate_must_be_after_business_opened_2019 check (year(FirstServiceDate) > 2018),
     Active bit not null default 1,
-    Constraint u_Pet_already_in_database unique (Ownername, OwnerAddress, PetType, PetName, FirstServiceDate)
+    Constraint u_Customer_Pet_already_in_database unique (Ownername, OwnerAddress, PetType, PetName, FirstServiceDate)
 )
--- 3 Reports.sql
+++ 3 Reports.sql
@@ -4,24 +4,21 @@ go
 Reports:
 --1)We need a list of how many of each pet we currently have.
 SELECT p.PetType, Amount = count(*)
-from Pet p 
+from Customer p 
 Where p.active = 1
 Group by p.PetType
 
 --2)we need to see how many customers have multiple pets.
-SELECT NumOfCustomers = Count(*) from (
-    select p.OwnerName, AmountOfPets = count(p.OwnerName)
-    from Pet p 
-    group by p.OwnerName
-    HAVING count(p.PetName) > 1
-) x
+select p.OwnerName, NumOfCustomers = count(p.OwnerName)
+from Customer p
+
 --3)We need to know our top customer - the customer that is paying us the most.
 SELECT top 1 TopPayingCustomer = p.OwnerName, TotalPays = SUM(case when p.Frequency = 'weekly' then p.Price *2 else p.Price end)
-from Pet p 
+from Customer p 
 group by p.OwnerName
 ORDER by TotalPays desc
 
 --4)We need to know the average amount we make per animal type 
 select p.PetType, AverageIncome = avg(case when p.Frequency = 'weekly' then p.Price *2 else p.Price end)
-from Pet p 
+from Customer p 
 group by p.PetType
