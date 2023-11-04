5b8a8bdb9d61478a9a14339dc7f0ca08a6e27efd
RiFinkelstein <rivkafinkelstein823@gmail.com>
2023-11-04 22:31:53 +0200

    added sketch

---
+++ sketch.sql
@@ -0,0 +1,41 @@
+/*
+    CustomerID 
+        int not null pk,
+    FirstName
+        varchar(50)
+        not null 
+        constraint ck_Customer_First_name_is_not_blank CHECK (FirstName <> ''),
+    LastName
+        varchar(50)
+        not null 
+        constraint ck_Customer_Last_name_is_not_blank CHECK (LastName <> ''),
+    PhoneNumber
+        int 
+        not null 
+        not blank
+        9 numbers,
+    Address
+        varchar(50)
+        not null 
+        not blank,
+    Frequency
+        varchar(8)
+        not null 
+        check if weekly or biweekly, 
+    PricePerHour
+        num
+        not more than 53,
+    Hours
+        num
+        not null,
+    StartDate
+        date, 
+        not null
+        not blank,
+    EndDate
+        date
+        null,
+    constraints: 
+        check start date is not afer end date
+        check that pricerperhour times hour is not more than 424.00
+*/
