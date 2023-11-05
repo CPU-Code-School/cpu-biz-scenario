80650bcc730d38ce48141a0c851fbd1aca18a703
RiFinkelstein <rivkafinkelstein823@gmail.com>
2023-11-05 21:22:06 +0200

    finishes reports

--- 3 reports.sql
+++ 3 reports.sql
@@ -3,12 +3,19 @@
    For this I need to see the customers phone number, last name and the amount of money they will have to pay.
    Please do it in this format:  last name, first name (phone number) - price of service.
 */
+SELECT DeepCleanCustomers= CONCAT(c.LastName, ' ', c.FirstName, ' (',c.PhoneNumber, ') - ', (c.PricePerHour*c.TimeinHours)  )
+FROM CleaningCompany C
+where c.PricePerHour >=35 
+
 
 /*
-I would like to know how many months I have been working for each current customer. 
+2) I would like to know how many months I have been working for each current customer. 
    If you could show the customers I have worked for for the longest time at the top, I would appreciate that.
    Please include a column formatted like this: last name - weekly/bi weekly and a column for number of months
 */
+SELECT Customer= concat(c.LastName, ' - ', c.Frequency), NumberOfMonths= DATEDIFF(month, c.StartDate, isnull(c.EndDate, CURRENT_TIMESTAMP))
+FROM CleaningCompany c
+ORDER BY NumberOfMonths desc
 
 --3) What is the average price I ask per hour for regular cleans and what is the average price per hour for deep cleans?
 SELECT AvergaePricePerHourRegularClean = avg(c.PricePerHour)
@@ -33,10 +40,10 @@ ORDER by AmountPerYear desc
 
 
 --5) How many customers did I gain in the year 2020 and how many did I lose?
-SELECT count(*) 
+SELECT CustomersGained2020= count(*) 
 FROM CleaningCompany c 
 WHERE DATEPART(year, c.StartDate) = 2020
 
-SELECT count(*) 
+SELECT CustomersLosr2020= count(*) 
 FROM CleaningCompany c 
 WHERE DATEPART(year, c.EndDate) = 2020
