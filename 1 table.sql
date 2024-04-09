77de57d01c1a792bdd8aea9f633163460e11e6da
mklugman <mklug232@gmail.com>
2024-04-09 13:09:23 -0400

    more corrections to table

--- 1 table.sql
+++ 1 table.sql
@@ -17,11 +17,12 @@ create table dbo.Costume(
         constraint c_Costume_Size_can_be_XS_S_M_L_or_XL check(Size in ('XS', 'S', 'M', 'L', 'XL')),
     AmountBought int not null 
         constraint c_Costume_Amount_bought_must_be_1_or_more check(AmountBought >= 1),
-    SoldPricePerCostume int not null, 
+    SoldPricePerCostume int not null
+        constraint c_Costume_Sold_Price_must_be_between_15_and_30 check(SoldPricePerCostume between 15 and 30), 
     DateBought date not null
         constraint c_Costume_Date_bought_must_be_after_January_1_2020_and_cannnot_be_future_date check(DateBought between 'January 1 2020' and getdate()),
-    DateSold date not null
-        constraint c_Costume_Date_Sold_can_be_after_January_1_2020_and_cannot_be_future_date check(DateSold between 'January 1 2020' and getdate()),
+    DateSold date not null,
+        --constraint c_Costume_Date_Sold_can_be_after_January_1_2020_and_cannot_be_future_date check(DateSold between 'January 1 2020' and getdate()),
     BoughtPricePerCostume as case 
                                 when Size = 'XS' then 15
                                 when Size = 'S' then 17
@@ -30,7 +31,8 @@ create table dbo.Costume(
                                 when Size = 'XL' then 25
                             end persisted,
     TotalPaid as AmountBought*SoldPricePerCostume persisted,
-    PaidFullPrice as case 
+    --PaidFullPrice bit 
+    /*              case 
                         when  (Size = 'XS' and SoldPricePerCostume = 20)
                             or (Size = 'S' and SoldPricePerCostume = 22)
                             or (Size = 'M' and SoldPricePerCostume = 25)
@@ -39,6 +41,7 @@ create table dbo.Costume(
                         then 'Yes'
                         else 'No'
                         end persisted,
+                        */
     constraint c_Costume_Sold_Price_must_be_greater_than_or_equal_to_Bought_Price_per_costume check(SoldPricePerCostume >= BoughtPricePerCostume),
     constraint c_Costume_Date_Bought_must_be_before_or_same_as_Date_Sold check(DateBought <= DateSold) 
     
