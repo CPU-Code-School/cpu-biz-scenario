775a5871f7190388339857385f19e24460aef9b0
Chanamindy <155999547+Chanamindy@users.noreply.github.com>
2024-05-15 19:12:28 -0400

    Added constraint for Passport nationality.

--- 1 table airline.sql
+++ 1 table airline.sql
@@ -18,7 +18,7 @@ create table dbo.Flight(
     PassportNumber char(9) null constraint ck_Flight_passport_number_must_be_9_digits_and_start_with_any_digit_except_0 check(PassportNumber like '[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' or PassportNumber is null),
     PassportIssueDate date null constraint ck_Flight_passport_issue_date_is_before_the_current_date check(PassportIssueDate <= getdate(),
     PassportExpiryDate date null constraint ck_Flight_passport_expiry_date_must_be_in_the_future check(PassportExpiryDate > getdate(),
-    PassportNationality varchar(25) null,
+    PassportNationality varchar(25) null constraint ck_Flight_passport_nationality_cannot_be_blank check(PassportNationality <> ''),
     constraint ck_Flight_passport_number_and_issue_date_and_expiry_date_and_nationality_must_all_be_blank_or_all_have_a_value 
         check((PassportNumber = '' and PassportIssueDate = '' and PassportExpiryDate = '' and PassportNationality = '') or (PassportNumber <> '' and PassportIssueDate <> '' and PassportExpiryDate <> '' and PassportNationality <> '')),
     constraint u_Flight_flight_number_and_passenger_first_name_and_passenger_last_name_all_must_be_unique unique(FlightNumber, PassengerFirstName, PassengerLastName),
