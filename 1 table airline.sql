bc32c9ad2a3ea5674af1af25102f4862a751533d
Chanamindy <155999547+Chanamindy@users.noreply.github.com>
2024-05-22 12:35:02 -0400

    Changed constraint on valid to fly.

--- 1 table airline.sql
+++ 1 table airline.sql
@@ -23,12 +23,12 @@ create table dbo.Flight(
         check((PassportNumber = null and PassportIssueDate = null and PassportExpiryDate = null and PassportNationality = null) or (PassportNumber <> null and PassportIssueDate <> null and PassportExpiryDate <> null and PassportNationality <> null)),
     constraint u_Flight_flight_number_and_passenger_first_name_and_passenger_last_name_all_must_be_unique unique(FlightNumber, PassengerFirstName, PassengerLastName),
     constraint ck_Flight_time_departing_must_be_before_time_arriving check(TimeDeparting < TimeArriving),
-    constraint ck_Flight_passport_expiry_date_is_10_years_after_passport_issue_date_for_those_that_are_sixteen_and_5_years_for_under_16
-        check(datediff(year, DOB, PassportIssueDate) >= 16 and datediff(year, PassportIssueDate, PassportExpiryDate) <= 10
-        or (datediff(year, DOB, PassportIssueDate) < 16 and datediff(year, PassportIssueDate, PassportExpiryDate) <= 5)),
+    constraint ck_Flight_passport_expiry_date_is_9_years_and_six_months_after_passport_issue_date_for_those_that_are_sixteen_and_5_years_for_under_16_and_that_it_is_valid_to_fly
+        check((datediff(year, DOB, PassportIssueDate) >= 16 and datediff(month, PassportIssueDate, PassportExpiryDate) <= 114 and (((DepartureCountry = ArrivalCountry or PassportNationality = ArrivalCountry) and TimeDeparting <= PassportExpiryDate) 
+        or ((DepartureCountry <> ArrivalCountry or PassportNationality <> ArrivalCountry) and datediff(month, TimeDeparting, PassportExpiryDate) > 5)))
+        or ((datediff(year, DOB, PassportIssueDate) < 16 and datediff(year, PassportIssueDate, PassportExpiryDate) <= 5)) and (((DepartureCountry = ArrivalCountry or PassportNationality = ArrivalCountry) and TimeDeparting <= PassportExpiryDate) 
+        or ((DepartureCountry <> ArrivalCountry or PassportNationality <> ArrivalCountry) and datediff(month, TimeDeparting, PassportExpiryDate) > 5))),
     constraint ck_Flight_passenger_age_must_be_between_16_and_90 check(datediff(year, DOB, TimeDeparting) between 16 and 90),
-    constraint ck_Flight_passport_cannot_be_used_to_fly check(((DepartureCountry = ArrivalCountry or PassportNationality = ArrivalCountry) and TimeDeparting <= PassportExpiryDate) 
-        or ((DepartureCountry <> ArrivalCountry or PassportNationality <> ArrivalCountry) and datediff(month, TimeDeparting, PassportExpiryDate) > 5)),
     constraint ck_Flight_passport_issue_date_must_be_before_the_passport_expiry_date check(PassportIssueDate < PassportExpiryDate),
     constraint ck_Flight_departure_airport_is_not_equal_to_arrival_airport check(DepartureAirport <> ArrivalAirport)
 )
