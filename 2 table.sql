bcf035bc82a24b3341de94e5e37aac86f0825537
eanteby <eanteby2@gmail.com>
2024-08-09 00:28:04 -0400

    added changes to table

--- 2 table.sql
+++ 2 table.sql
@@ -51,7 +51,8 @@ create table dbo.Passenger(
 	ArrivalCountry varchar(30) not null
 		constraint ck_Passenger_ArrivalCountry_cannot_be_blank check (ArrivalCountry > ''),
 	TimeDeparting datetime not null
-		constraint ck_Passenger_TimeDeparting_cannot_be_more_than_one_year_past_current_date check (getdate() - TimeDeparting <= 00/00/0001),
+		constraint ck_Passenger_TimeDeparting_cannot_be_more_than_one_year_after_current_date check (TimeDeparting <= dateadd(year, 1, getdate())),
+		--I would add that it cannot be a past date, but the sample data is old
 	TimeArriving datetime not null,
 	PassengerFirstName varchar(30) not null
 		constraint ck_Passenger_PassengerFirstName_cannot_be_blank check (PassengerFirstName > ''),
@@ -60,34 +61,46 @@ create table dbo.Passenger(
 	PassengerDateOfBirth date not null, 
 	PassengerAddress varchar(150) not null 
 		constraint ck_Passenger_PassengerAddress_cannot_be_blank check (PassengerAddress > ''),
-	PassportNumber char(9) not null
-		constraint ck_PassportNumber_must_be_9_numbers check (PassportNumber like '[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
-		constraint u_Passenger_PassportNumber_must_be_unique unique
-		default '',
-	PassportIssueDate date not null 
-		default '',
-	PassportExpiryDate date not null 
-		default '', 
-	Nationality varchar(30) not null 
-		default '',
-	constraint u_Passenger_PassengerFirstName_PassengerLastName_PassengerDateOfBirth_PassengerAddress_and_FlightNumber_must_be_unique 
+	PassportNumber char(9) null
+		constraint ck_PassportNumber_must_be_9_numbers_or_null check (PassportNumber like '[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+	PassportIssueDate date null,
+	PassportExpiryDate date null, 
+	Nationality varchar(30) null,
+	--Is there a point in preventing blanks if I allow nulls?
+	constraint u_Passenger_PassengerFirstName_PassengerLastName_PassengerDateOfBirth_PassengerAdress_and_FlightNumber_must_be_unique 
 		unique (PassengerFirstName, PassengerLastName, PassengerDateOfBirth, PassengerAddress, FlightNumber),
 	constraint ck_Passenger_TimeArriving_must_be_greater_than_TimeDeparting 
 		check (TimeArriving > TimeDeparting),
-	constraint ck_Passport_Passport_Information_must_be_all_blank_or_all_entered 
-		check ((PassportNumber = '' and PassportIssueDate = '' and PassportExpiryDate = '' and Nationality = '') or (PassportNumber > '' and PassportIssueDate > '' and PassportExpiryDate > '' and Nationality > '')),
+	constraint ck_Passport_Passport_Information_must_be_all_null_or_all_entered 
+		check ((PassportNumber is null and PassportIssueDate is null and PassportExpiryDate is null and Nationality is null) or (PassportNumber is not null and PassportIssueDate is not null and PassportExpiryDate is not null and Nationality is not null)),
+		--if I allow blanks for passport info, do I have to add a constraint so that everything is null or blank or everything is entered?
+		--or is it enough to just have a constraint that allows all null or all not null?
 	constraint ck_Passport_DepartureAirport_cannot_be_the_same_as_ArrivalAirport 
 		check (DepartureAirport <> ArrivalAirport),
 	constraint ck_Passenger_Passenger_age_must_be_beween_16_and_90 
-		check (datediff (year, PassengerDateOfBirth, TimeDeparting) between 16 and 19),
-	constraint ck_Passenger__IssueDate_cannot_be_more_than_9_years_and_6_months_before_flight
+		check (datediff (year, PassengerDateOfBirth, TimeDeparting) between 16 and 90),
+	constraint ck_Passenger_PassportIssueDate_cannot_be_before_of_valid_time_frame
 		check 
 			(
-			(ArrivalCountry not in (DepartureCountry, Nationality) and datediff(year, PassengerDateOfBirth, TimeDeparting) < 16 and TimeDeparting < dateadd(year, 9, dateadd(month, 6,PassportIssueDate)))
-			or (ArrivalCountry not in (DepartureCountry, Nationality) and datediff(year, PassengerDateOfBirth, TimeDeparting) >= 16 and TimeDeparting < dateadd(year, 5, PassportIssueDate))
-			or (ArrivalCountry in (DepartureCountry, Nationality) and datediff(year, PassengerDateOfBirth, TimeDeparting) >= 16 and TimeDeparting < PassportExpiryDate)
+				(	
+					(datediff(year, PassengerDateOfBirth, PassportIssueDate) >= 16)
+					and
+					(
+						(ArrivalCountry not in (DepartureCountry, Nationality)
+						and TimeDeparting < dateadd(year, 9, dateadd(month, 6,PassportIssueDate)))
+						or
+						ArrivalCountry in (DepartureCountry, Nationality) 
+					)
+				)
+				or 
+				(
+					datediff(year, PassengerDateOfBirth, PassportIssueDate) < 16 
+					and  TimeDeparting < dateadd(year, 5, PassportIssueDate)
+						
+				)
 			),
-	constraint ck_Passenger_PassportIssueDate_must_be_between_PassengerDateOfBirth_and_PassportExpiryDate check (PassportIssueDate between PassengerDateOfBirth and PassportExpiryDate)
+	constraint ck_Passenger_PassportIssueDate_must_be_between_PassengerDateOfBirth_and_TimeDeparting check ((PassportIssueDate between PassengerDateOfBirth and TimeDeparting) or PassportIssueDate = ''),
+	constraint ck_Passport_cannot_be_expired check (TimeDeparting < PassportExpiryDate)
  
  )	
 
