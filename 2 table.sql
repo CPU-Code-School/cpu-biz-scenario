48e9666d358c0418a8375c1bde08156ea47420d2
eanteby <eanteby2@gmail.com>
2024-08-11 17:47:03 -0400

    changes to table and reports

--- 2 table.sql
+++ 2 table.sql
@@ -1,39 +1,3 @@
-/*
-Flight number, Departure Airport, Country, Arrival Airport, Country, Time Departing, 
-Time Arriving, Name, DOB (dd/mm/yyyy), Address, (Passport number, Issue date, Expiry date, Nationality) if provided
-
-
-
-use airlineDB
-go 
-drop table if exists dbo.Passenger
-go 
-create table dbo.Passenger(
-	PassengerID int not null identity primary key,
-	FlightNumber char(6) constraint flightnumber must be FLY[0-9][0-9][1-9]
-	DepartureAirport char(3) not null must be [a-z][a-z][a-z]
-	DepartureCountry varchar(30) not null not blank
-	ArrivalAirport char(3) not null must be [a-z][a-z][a-z]
-	ArrivalCountry varchar(30) not null not blank
-	TimeDeparting can't be more than one year past current date
-	TimeArriving 
-	PassengerFirstName varchar(30) not null not blank
-	PassengerLastName varchar(30) not null not blank
-	PassengerDateOfBirth date not null 16-90
-	PassengerAddress varchar(150) not null not blank
-	PassportNumber char(9) not null default " must be  [1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9] unique- test if could enter null
-	PassportIssueDate date not null default " when international and to passports country, can't be more than 9 yrs and 6 mo old 
-		when age < 16, can't be more than 5 years old
-	PassportExpiryDate date not null default " 
-	Nationality not null default "
-	multi- column constraints
-		name, dob, address and flight # must be unique
-		time arriving must be greater than time departing
-		passport info must be all entered or all blank
-)
-
-*/
-
 use airlineDB
 go 
 drop table if exists dbo.Passenger
@@ -41,7 +5,7 @@ go
 create table dbo.Passenger(
 	PassengerID int not null identity primary key,
 	FlightNumber  char(6) not null 
-		constraint ck_Passenger_FlightNumber_must_be_FLY_followed_by_3_numbers check (FlightNumber like 'FLY[0-9][0-9][1-9]'),
+		constraint ck_Passenger_FlightNumber_must_be_FLY_followed_by_3_numbers_starting_at_001 check (FlightNumber like 'FLY[0-9][0-9][0-9]' and FlightNumber not like 'FLY000'),
 	DepartureAirport char(3) not null 
 		constraint ck_Passenger_DepartureAirport_must_be_3_letters check (DepartureAirport like '[a-z][a-z][a-z]'),
 	DepartureCountry varchar(30) not null 
@@ -65,7 +29,8 @@ create table dbo.Passenger(
 		constraint ck_PassportNumber_must_be_9_numbers_or_null check (PassportNumber like '[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
 	PassportIssueDate date null,
 	PassportExpiryDate date null, 
-	Nationality varchar(30) null,
+	Nationality varchar(30) null
+		constraint ck_Passenger_Nationality_cannot_be_blank check (Nationality > ''),
 	--Is there a point in preventing blanks if I allow nulls?
 	constraint u_Passenger_PassengerFirstName_PassengerLastName_PassengerDateOfBirth_PassengerAdress_and_FlightNumber_must_be_unique 
 		unique (PassengerFirstName, PassengerLastName, PassengerDateOfBirth, PassengerAddress, FlightNumber),
@@ -73,8 +38,6 @@ create table dbo.Passenger(
 		check (TimeArriving > TimeDeparting),
 	constraint ck_Passport_Passport_Information_must_be_all_null_or_all_entered 
 		check ((PassportNumber is null and PassportIssueDate is null and PassportExpiryDate is null and Nationality is null) or (PassportNumber is not null and PassportIssueDate is not null and PassportExpiryDate is not null and Nationality is not null)),
-		--if I allow blanks for passport info, do I have to add a constraint so that everything is null or blank or everything is entered?
-		--or is it enough to just have a constraint that allows all null or all not null?
 	constraint ck_Passport_DepartureAirport_cannot_be_the_same_as_ArrivalAirport 
 		check (DepartureAirport <> ArrivalAirport),
 	constraint ck_Passenger_Passenger_age_must_be_beween_16_and_90 
@@ -86,20 +49,16 @@ create table dbo.Passenger(
 					(datediff(year, PassengerDateOfBirth, PassportIssueDate) >= 16)
 					and
 					(
-						(ArrivalCountry not in (DepartureCountry, Nationality)
-						and TimeDeparting < dateadd(year, 9, dateadd(month, 6,PassportIssueDate)))
+						TimeDeparting < dateadd(year, 9, dateadd(month, 6,PassportIssueDate))
 						or
 						ArrivalCountry in (DepartureCountry, Nationality) 
 					)
 				)
 				or 
-				(
-					datediff(year, PassengerDateOfBirth, PassportIssueDate) < 16 
-					and  TimeDeparting < dateadd(year, 5, PassportIssueDate)
-						
-				)
+				(TimeDeparting < dateadd(year, 5, PassportIssueDate))
+ 
 			),
-	constraint ck_Passenger_PassportIssueDate_must_be_between_PassengerDateOfBirth_and_TimeDeparting check ((PassportIssueDate between PassengerDateOfBirth and TimeDeparting) or PassportIssueDate = ''),
+	constraint ck_Passenger_PassportIssueDate_must_be_between_PassengerDateOfBirth_and_TimeDeparting check ((PassportIssueDate between PassengerDateOfBirth and TimeDeparting)),
 	constraint ck_Passport_cannot_be_expired check (TimeDeparting < PassportExpiryDate)
  
  )	
--- 4 reports.sql
+++ 4 reports.sql
@@ -2,7 +2,7 @@
 -- 1) How many people are booked per flight as: count of people, flight number, departure airport, when the flight is departing and destination.
 select p.FlightNumber, P.DepartureAirport, p.TimeDeparting, p.ArrivalAirport, AmountOfPassengers = count(*)
 from Passenger p 
-group by p.FlightNumber, P.DepartureAirport, p.TimeDeparting, p.
+group by p.FlightNumber, P.DepartureAirport, p.TimeDeparting, p.ArrivalAirport
 
 -- 2) Who isn't checked in for flights departing in the next week, in order to send them reminders to check in.
 select * 
@@ -12,22 +12,14 @@ and TimeDeparting < dateadd(week, 1, getdate())
 
 -- 3) How many flights are departing per day, and num of passengers we have on those flights.
 --was it possible to do this in one report?
-select DepartureDate = convert(varchar, p.TimeDeparting, 103), p.FlightNumber, NumPassengers = Count(*)
+select DepartureDate = convert(varchar, p.TimeDeparting, 103), NumPassengers = Count(*), FlightsDeparting = count(distinct p.FlightNumber)
 from Passenger p 
-group by convert(varchar, p.TimeDeparting, 103), p.FlightNumber
-
-select DepartureDate = convert(varchar, p.TimeDeparting, 103), FlightsDeparting = count(distinct p.FlightNumber)
-from Passenger p
 group by convert(varchar, p.TimeDeparting, 103)
 
 -- 4) How many flights are departing per destination, and num of passengers we have on those flights, to know what route is attracts most people.
 --not sure how to do this in one report
-select p.ArrivalAirport, p.FlightNumber, NumPassengers = Count(*)
+select p.ArrivalAirport, NumPassengers = Count(*), NumberOfFlights = count(distinct p.FlightNumber)
 from Passenger p 
-group by p.ArrivalAirport, p.FlightNumber
-
-select p.ArrivalAirport, NumberOfFlights = count(distinct p.FlightNumber)
-from Passenger p
 group by p.ArrivalAirport
 
 -- 5) How many people booked but didn't actually travel in the end.
