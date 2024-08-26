738ed3f94a20eaf7e069ce3bf47488a6f386ec08
AvigailGellis <agellis@cpucodeschool.com>
2024-08-26 10:15:54 +0300

    Removed and added new biz scenarios

---
+++ bike shop.sql
@@ -0,0 +1,55 @@
+/*
+Hi, I am the owner of a bicycle shop located in Spring Valley, NY. I opened my shop in June of the year 2022. I sell new and used bikes to a variety of customers. The second hand bikes I sell are fixed up to as good as new before being sold. 
+It has been getting too hard to keep track of my sales in my trusty notebook so I was referred to your company to help me out and keep track of my orders electronically. I am confident that you will do an excellent job.
+
+For each bike sold I need the customers name, address and phone number.
+It's important for me to keep track of the bike company, size and color. I need to record the date I received the bike, the date I sold it and in what season I sold the bike. I need to know if I am selling a new or second hand bike. 
+If the bike was used and I am reselling it I want to record the condition it was in when I received it. The purchase price and the price I sold the bike for needs to be recorded in the table. 
+
+These are the four conditions I receive used bikes in:
+1: Perfect
+2: Minor Fixup
+3: Major Fixup
+4: Restoration
+
+
+Questions:
+Q: Do you ever sell a bike for less than you paid for it?
+A: I prefer not, but if I have a bike sitting for a long time I may have to sell it for cheaper than what I paid for it.
+
+Q: Can one customer buy more than one bike?
+A: Yes, however it is recorded separately for each bike, they can buy 2 bikes on the same day.
+
+Q: Do you ever sell a bike before you receive it?
+A: No.
+
+Q: What is the most you ever sold a bike for?
+A: My highest selling price won't be more than $3,000.00
+
+Q: Did you buy any bikes before you opened your store?
+A: I started buying bikes three months before I opened my store.
+
+
+Reports:
+1) I would like to know how many local customers I have as oppose to out-of-town customers.
+2) The amount of bikes sold per season to know what is my busiest season.
+3) I need a report with the average, minimum and maximum amount of time a bike was in my store before I sold it. Also show in this report my total profit so far.
+4) I need a report with the profit for each sale and show the Customers name, BikeCompany, PurchasePrice, SoldPrice, New/Used.
+5) Which is the most popular bike company I sell.
+
+
+Sample Data:
+CustomerName, Address, PhoneNumber, BikeCompany, BikeSize, BikeColor, DatePurchased, PurchasePrice, New, ConditionWhenReceived, DateSold, Season, SoldPrice
+Shmuel Bitton, 4 Sparrow Drive Spring Valley NY 10977, 845-425-9501, Schwinn, 24", Black, 2022-07-20, $110.00, New, 2022-09-15, Summer, $220.00
+Jack Sullivan, 1889 Fifty Second Street Brooklyn NY 11218, 718-350-4401, Trek, 24", Gray, 2023-01-26, $150.00, New, 2023-05-11, Spring, $250.00
+Rochel Cohen, 95 Francis Place Spring Valley NY 10977, 845-371-2052, Huffy, 16", Pink, 2023-03-13, $30.00, New, 2023-06-18, Spring, $85.00 
+Meir Stern, 7 Bluejay Street Spring Valley NY 10977, 845-426-9806, Razor, 20", Slate, 2023-08-06, $17.00, Used, Restoration, 2023-10-26, Fall, $61.00
+Yehuda Gluck, 11 Parness Rd. #3	South Fallsburg	NY 12733, 845-434-4011, Kent, 26", Black, 2024-01-08, $120.00, New, 2024-02-19, Winter, $250.00
+Gedallia Gold, 2036 Park Avenue Lakewood NJ 08701, 732-930-6402, Trek, 20", Blue, 2022-05-12, $105.00, Used, Minor Fixup, 2024-02-07, Winter, $200.00
+Binyomin Shapiro, 66 Carlton Road Monsey NY 10952, 845-356-9027, Schwinn, 26", Gray, 2022-04-22, $150.00, Used, Perfect, 2024-01-09, Winter, $135.00
+Malka Fischer, 80 Twin Avenue Spring Valley NY 10977, 845-425-9002, Malibu, 18", Pink, 2022-12-04, $90.00, New, 2023-06-23, Summer, $120.00
+Yonason Katz, 1470 E 26th Street Brooklyn NY 11223, 718-376-2658, Huffy, 20", Blue, 2023-06-14, $76.00, New, 2023-08-03, Summer, $130.00
+Bracha Smith, 25 North Rigaud Road Spring Valley NY 10977, 845-352-1099, Razor, 24", Slate, 2023-05-18, $167.00, New, 2023-07-22, Summer, $220.00
+Moshe Weiss, 25 Old Nyack Turnpike Monsey NY 10952, 845-356-9423, Kent, 24", Black, 2022-12-13, $103.00, Used Minor Fixup, 2023-08-16, Summer, $195.00
+Yehuda Jacobs, 1650 Lexington Avenue Lakewood NJ 08701, 732-930-8054, Schwinn, 20", Blue, 2022-04-09, $42.00, Used, Major Fixup, 2022-07-20, Summer, $98.00
+*/
