fe2ef7492d89d9fdb78fe262630c8506a984a411
SMajerovic <shiframajerovic@gmail.com>
2025-01-13 20:35:08 -0500

    fixed table and queries

--- 1 table.sql
+++ 1 table.sql
@@ -6,9 +6,9 @@ create table dbo.Costume (
     CostumeId int not null identity primary key,
     CustomerLastName varchar (25) not null constraint ck_customer_last_name_cannot_be_blank check (CustomerLastName <> ''),
     CustomerFirstName varchar (25) not null constraint ck_customer_first_name_cannot_be_blank check (CustomerFirstName <> ''),
-    CostumeBought varchar (50) not null constraint ck_costume_bought_cannot_be_blank check (CostumeBought <> ''),
+    CostumeBought varchar (50) not null constraint ck_costume_bought_can_only_be_from_a_select_costume_type check (CostumeBought in ('american girl doll', 'artist', 'bumble bee', 'colonial boy', 'colonial girl', 'elephant', 'fire man', 'police man', 'zebra', 'princess')),
     CostumeSize varchar (2) not null constraint ck_costume_size_can_only_be_xs_s_m_l_or_xl check (CostumeSize in ('xs','s','m','l','xl')),
-    AmountBought int not null,
+    AmountBought int not null constraint ck_amount_bought_must_be_greater_than_zero check (AmountBought > 0),
     CostPricePerCostume as case CostumeSize 
         when 'xs' then 15
         when 's' then 17
@@ -25,15 +25,11 @@ create table dbo.Costume (
         end persisted,
     PricePaidByCustomer int not null constraint ck_price_paid_by_costomer_must_be_between_15_and_30 check(PricePaidByCustomer between 15 and 30),
     TotalPaidByCustomer as PricePaidbyCustomer * AmountBought persisted,
-    FullPricePaid bit,
-    FullPricePaidDesc as case FullPricePaid
-        when 0 then 'false'
-        when 1 then 'true'
-        end persisted,
+    FullPricePaid bit not null,
     DateBought date not null constraint ck_date_bought_cannot_be_earlier_than_january_1_2020 check(DateBought >= '2020-1-1'),
         constraint ck_date_bought_cannot_be_a_future_date check (DateBought <= getdate()),
-    DateSold date not null constraint ck_date_sold_cannot_be_earlier_than_january_1_2020 check (DateSold >= '2020-1-1'),
-        constraint ck_date_sold_cannot_be_a_future_date check (DateSold <= getdate())
+    DateSold date not null constraint ck_date_sold_cannot_be_a_future_date check (DateSold <= getdate()),
+        constraint ck_date_sold_cannot_be_before_date_bought check (DateSold >= DateBought)
 )
 
 
--- 3 queries.sql
+++ 3 queries.sql
@@ -1,27 +1,26 @@
 -- 1. I need to know which costume is the most popular.
-select CostumeBought, TotalAmountPurchased = count(CostumeBought)
+select top 1 CostumeBought, TotalAmountPurchased = count(CostumeBought)
 from Costume c
 group by CostumeBought
-order by count(*) desc 
+order by TotalAmountPurchased desc 
 
  --2. I need to know which size is the most popular.
-select CostumeSize, TotalAmountPurchasedInSize = count (CostumeSize)
+select top 1 CostumeSize, TotalAmountPurchasedInSize = count (CostumeSize)
 from Costume c
 group by CostumeSize
-order by count(*) desc 
+order by TotalAmountPurchasedInSize desc 
 
 
 
 -- 3. I need you to show me all of my customers in the following format: name: amountbought - costume customer bought (how much they paid)
-select InfoPerPurchase = concat(CustomerFirstName, ' ', CustomerLastName, ': ', AmountBought, ' - ', CostumeBought, ' (', PricePaidByCustomer, ')') 
+select InfoPerPurchase = concat(c.CustomerFirstName, ' ', c.CustomerLastName, ': ', c.AmountBought, ' - ', c.CostumeBought, ' (', c.TotalPaidByCustomer, ')') 
 from costume c
 
 
 
  --4. I need to know the profit each sale.
-select c.CustomerLastName, c.CustomerFirstName, c.CostumeBought, ProfitOfSale = TotalPaidByCustomer - CostPricePerCostume
+select c.CustomerLastName, c.CustomerFirstName, c.CostumeBought, ProfitOfSale = c.TotalPaidByCustomer - c.CostPricePerCostume * c.AmountBought
 from Costume c 
-
 
 
 
