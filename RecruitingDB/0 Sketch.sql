b4797788be30b33c30d68e259df5f48515a1740d
rmathis <ryanmathis@ccimail.com>
2025-02-10 16:48:54 -0500

    partially done work

---
+++ RecruitingDB/0 Sketch.sql
@@ -0,0 +1,14 @@
+RecruitingDB
+
+Educators
+    EducatorID int not null identity primary key,
+    FirstName varchar(30) not null not blank
+    LastName varchar(30) not null not blank
+    DOB date not before today
+    Gender varchar(1) not null either M or F
+    CollegeAttended varchar(40) not null not blank
+    DegreeTitle varchar(30) not null not blank
+    Media varchar(20) not null not blank
+    DateContracted date not null not before Feb 17, 2017
+    SchoolPlaced varchar(40) null not blank
+    DateFoundJob date null must be after Date contracted
---
+++ RecruitingDB/1 Database.sql
@@ -0,0 +1,6 @@
+use master
+go
+drop database if exists RecruitingDB
+go
+create database RecruitingDB
+go
