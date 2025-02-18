e985cc2211e016211d904a25f1f9661146a21d85
BZaidman <benzionzaidman@gmail.com>
2025-02-18 14:04:08 -0500

    fixed 2 comments

--- 1 table sales.sql
+++ 1 table sales.sql
@@ -10,9 +10,10 @@ create table dbo.Sales(
 		constraint c_sales_customer_address_cannot_be_blank check(CustomerAddress > ''),   
 	City varchar(15) not null
 		constraint c_sales_city_cannot_be_blank check(City > ''),
-	State varchar(15) not null
+	State char(2) not null
 		constraint c_sales_state_cannot_be_blank check(State > ''),
 	ZipCode Varchar(7) not null
+		constraint c_sales_zip_code_must_be_numeric check(ZipCode not like '%[^0-9]%'),
 		constraint c_sales_zip_code_cannot_be_blank check(ZipCode > ''),
 	CustomerPhoneNumber varchar (16) not null
 		constraint c_sales_customer_phone_number_cannot_be_blank check(CustomerPhoneNumber > ''),
