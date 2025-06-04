1cb3cae89784652462ed72ed30e56655f8a7cd6f
mordechaigellis <73965707+mordechaigellis@users.noreply.github.com>
2025-06-04 10:47:28 -0400

    Update recruiting educators company.sql

--- recruiting educators company.sql
+++ recruiting educators company.sql
@@ -1,78 +1,40 @@
-# Educators Connect - Recruiting Data Overview
+I have a recruiting company called Educators Connect, which specializes in helping educators find jobs, mainly right after graduating. 
+Every day we place many educators in different schools all over New York. 
+In order to conduct data analysis, we want to keep track of our educators and various details about them. 
 
-Educators Connect is a recruiting company that specializes in helping educators find jobs, mainly right after graduating.  
-Every day, we place many educators in different schools all over New York.  
-To conduct data analysis, we keep track of our educators and various details about them.
+I need to know the first name, last name, date of birth, gender, college attended, and title of their degree. 
+Then I need to know how they found out about recruiting company, the date they contacted us, which school we placed them in and the date we found them a job. 
 
-## Tracked Information
+Reports:
+1. I need to know how many students from each college we were able to find jobs for in under 2 weeks. 
+    This will help my company know which college's graduates we should try to attract to our company.
+2. Who were we more successful in placing, males or females?
+3. On average, how many people contact us every day and how many people fnd out about us per form of media. 
+    This will help us decide which forms of media communication we should invest in.
+4. On average, how many people do we succeed in placing per day?
+5. How many educators get placed a day per type of education degree?
+6. I need a list of the educators who reach out to us in the format of first name, last name, age, - degree.
 
-We collect the following data for each educator:
+Question: Do you deal with all age brackets?
+Answer: yes
+Question: What are the forms of media you use to advertise?
+Answer: We post ads about our company in magazines, newspapers, and social media sites. Sometimes people hear about us by word of mouth.
+Question: Do you specifically help graduates who graduated from a New York College?
+Answer: Not specifically. We help people who graduated from any college in the United States, but are living in New York. 
+Question: How long has your company been around?
+Answer: I opened up about 5 years ago, on February 17, 2017.
+Question: Are there any educators whom as of now, you have not succeeded in placing?
+Answer: Unfortunately yes. This is why I would like you to conduct reports to see the factors specific to educators we struggle to place.
+Question: Is there a certain degree level you mainly cater to?
+Answer: No. We help those with any level of education degree, whether it's a bachechelors, masters, or PhD
 
-- First Name  
-- Last Name  
-- Date of Birth  
-- Gender  
-- College Attended  
-- Title of Degree  
-- How they found out about our recruiting company  
-- Date they contacted us  
-- School we placed them in  
-- Date we found them a job  
-
-## Reports Needed
-
-1. **Placement Speed by College**  
-   - How many students from each college were placed in under 2 weeks?  
-   - Helps determine which colleges’ graduates we should attract.
-
-2. **Placement Success by Gender**  
-   - Are we more successful in placing males or females?
-
-3. **Daily Contact and Media Source Analysis**  
-   - On average:
-     - How many people contact us per day?
-     - How many people find out about us via each form of media?
-   - Helps guide media investment.
-
-4. **Daily Placement Rate**  
-   - On average, how many people do we succeed in placing per day?
-
-5. **Placement by Degree Type**  
-   - How many educators are placed per day per type of education degree?
-
-6. **Educator Contact List**  
-   - List of educators who reach out to us in the format:  
-     `First Name, Last Name, Age – Degree`
-
-## FAQ
-
-**Q: Do you deal with all age brackets?**  
-**A:** Yes
-
-**Q: What are the forms of media you use to advertise?**  
-**A:** We post ads in magazines, newspapers, and social media. Some hear about us via word of mouth.
-
-**Q: Do you specifically help graduates from New York colleges?**  
-**A:** Not specifically. We help graduates from any U.S. college, as long as they are living in New York.
-
-**Q: How long has your company been around?**  
-**A:** Since February 17, 2017.
-
-**Q: Are there any educators you haven’t succeeded in placing?**  
-**A:** Unfortunately, yes. We want to analyze the data to understand why.
-
-**Q: Is there a certain degree level you mainly cater to?**  
-**A:** No. We assist educators with all degree levels — Bachelor’s, Master’s, or PhD.
-
-## Sample Data
-
-| First Name | Last Name | DOB        | Gender | College Attended        | Title of Degree                 | Media           | Date Contacted | School Placed              | Date Found Job |
-|------------|------------|------------|--------|--------------------------|----------------------------------|------------------|----------------|-----------------------------|----------------|
-| Mary       | Lynn       | 9/13/2000  | Female | Excelsior College        | BA in Mathematics Education     | Magazine         | 5/2/2022       | Brooklyn High School        | 5/9/2022       |
-| Josh       | Frank      | 4/23/1998  | Male   | Georgia State University | MA in Social Studies Education  | Social Media     | 2/12/2022      | Manhattan Elementary School | 5/9/2022       |
-| Charles    | Smith      | 7/9/1994   | Male   | Excelsior College        | PhD in Education                | Social Media     | 8/7/2021       | New York City Day School    | 8/12/2021      |
-| Samantha   | Brown      | 9/24/1999  | Female | Columbia University      | BA in English Education         | Newspaper        | 5/23/2021      | Brooklyn High School        | 7/30/2021      |
-| Howard     | Lang       | 8/4/1998   | Male   | Georgia State University | MA in History Education         | Word of Mouth    | 1/31/2022      | null                        | null           |
-| Sarah      | Blanks     | 10/20/1995 | Female | Columbia University      | MA in Science Education         | Social Media     | 5/23/2020      | New York City Day School    | 8/17/2020      |
-| Ella       | Lewis      | 8/22/2000  | Female | Excelsior College        | BA in English Education         | Word of Mouth    | 4/1/2022       | null                        | null           |
-| Julie      | Goldman    | 3/30/1997  | Female | University of Denver     | MA in Social Studies Education  | Social Media     | 7/14/2020      | Manhattan Elementary School | 8/17/2020      |
+Sample Data:
+First name, last name, DOB, gender, college attended, title of degree, Media, date contacted, school placed, date found job
+Mary, Lynn, 9/13/2000, female, Excelsior College, BA in Mathematics Education, magazine, 5/2/2022, Brooklyn High School, 5/9/2022
+Josh, Frank, 4/23/1998, male, Georgia State University, MA in Social Studies Education, social media site, 2/12/2022, Manhattan Elementary School, 5/9/2022
+Charles, Smith, 7/9/1994, male, Excelsior College, PhD in Education, social media site, 8/7/2021, New York City Day School, 8/12/2021
+Samantha, Brown, 9/24/1999, female, Columbia University, BA in English Education, newspaper, 5/23/2021, Brooklyn High School, 7/30/2021
+Howard, Lang, 8/4/1998, male, Georgia State University, MA in History Education, word of mouth, 1/31/2022, null, null
+Sarah, Blanks, 10/20/1995, female, Columbia University, MA in Science Education, social media, 5/23/2020, New York City Day School, 8/17/2020
+Ella, Lewis, 8/22/2000, female, Excelsior College, BA in English Education, word of mouth, 4/1/2022, null, null
+Julie, Goldman, 3/30/1997, University of Denver, MA in Social Studies Education, social media, 7/14/2020, Manhattan Elementary School, 8/17/2020
