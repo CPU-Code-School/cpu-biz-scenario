da5add16897125317fc4b828b2dfc2a2ca5ad40f
faiga-new <faigaeinhorn@gmail.com>
2025-07-22 12:39:17 -0400

    implemented the bike shop scenario

--- 1 table.sql
+++ 1 table.sql
@@ -1,1 +1,32 @@
-Create table dbo....
+use master
+go
+drop database if exists BikeDB
+go
+create database BikeDB
+go
+use BikeDB
+go
+drop table if exists BikeShop
+go
+create table dbo.BikeShop(
+BikeId int not null identity primary key,
+CustomerName varchar(50) not null constraint ck_Bike_customername_cannot_be_blank check(CustomerName <> ' '),
+CustomerAddress varchar(100) not null constraint ck_Bike_customeraddress_cannot_be_blank check(CustomerAddress <> ' '),
+PhoneNumber varchar(15) not null constraint ck_Bike_phonenumber_cannot_be_blank check(PhoneNumber <> ' '),
+BikeCompany varchar(10) not null constraint ck_Bike_bikecompany_cannot_be_blank check(BikeCompany <> ' '),
+BikeSize int not null constraint ck_Bike_bikesize_must_be_greater_than_0 check(BikeSize > 0),
+BikeColor varchar(10) not null constraint ck_Bike_bikecolor_cannot_be_blank check(BikeColor <> ' '),
+DatePurchased datetime not null constraint ck_Bike_datepurchased_must_be_after_February_2022 check(DatePurchased > 02-29-2022),
+PurchasePrice decimal(6,2) not null constraint ck_Bike_purchaseprice_must_be_greater_than_0 check(PurchasePrice > 0),
+New varchar(4) not null constraint ck_Bike_new_must_be_either_new_or_used check(New in ('New', 'Used')),
+ConditionWhenReceived varchar(12) null,
+constraint ck_Bike_conditionwhenreceived_must_either_of_4_options_or_blank check(ConditionWhenReceived in ('Perfect', 'Minor Fixup', 'Major Fixup', 'Restoration', ' ')),
+DateSold datetime not null,
+SeasonSold varchar(8) not null constraint ck_Bike_seasonsold_cannot_be_blank check(SeasonSold <> ' '),
+SoldPrice decimal(6,2) not null constraint ck_Bike_soldprice_must_be_between_0_and_3000 check(SoldPrice between 0 and 3000),
+constraint ck_Bike_datesold_must_be_after_datepurchased check(DateSold >= DatePurchased),
+constraint ck_Bike_conditionwhenreceived_and_New_must_align check((New = 'Used' and ConditionWhenReceived <> ' ' and ConditionWhenReceived is not null) or (New = 'New' and (ConditionWhenReceived = ' ' or ConditionWhenReceived is null)))
+)
+
+select * from BikeShop
+
---
+++ 2 Data.sql
@@ -0,0 +1,15 @@
+use BikeDB
+go
+insert BikeShop(CustomerName, CustomerAddress, PhoneNumber, BikeCompany, BikeSize, BikeColor, DatePurchased, PurchasePrice, New, ConditionWhenReceived, DateSold, SeasonSold, SoldPrice)
+select 'Shmuel Bitton', '4 Sparrow Drive Spring Valley NY 10977', '845-425-9501', 'Schwinn', 24, 'Black', '07/20/2022', 110.00, 'New', '', '09/15/2022', 'Summer', 220.00
+union select 'Jack Sullivan', '1889 Fifty Second Street Brooklyn NY 11218', '718-350-4401', 'Trek', 24, 'Gray', '01/26/2023', 150.00, 'New', '', '05/11/2023', 'Spring', 250.00
+union select 'Rochel Cohen', '95 Francis Place Spring Valley NY 10977', '845-371-2052', 'Huffy', 16, 'Pink', '03/13/2023', 30.00, 'New', '', '06/18/2023', 'Spring', 85.00 
+union select 'Meir Stern', '7 Bluejay Street Spring Valley NY 10977', '845-426-9806', 'Razor', 20, 'Slate', '08/06/2023', 17.00, 'Used', 'Restoration', '10/26/2023', 'Fall', 61.00
+union select 'Yehuda Gluck', '11 Parness Rd. #3	South Fallsburg	NY 12733', '845-434-4011', 'Kent', 26, 'Black', '01/08/2024', 120.00, 'New', '', '02/19/2024', 'Winter', 250.00
+union select 'Gedallia Gold', '2036 Park Avenue Lakewood NJ 08701', '732-930-6402', 'Trek', 20, 'Blue', '05/12/2022', 105.00, 'Used', 'Minor Fixup', '02/07/2024', 'Winter', 200.00
+union select 'Binyomin Shapiro', '66 Carlton Road Monsey NY 10952', '845-356-9027', 'Schwinn', 26, 'Gray', '04/22/2022', 150.00, 'Used', 'Perfect', '01/09/2024', 'Winter', 135.00
+union select 'Malka Fischer', '80 Twin Avenue Spring Valley NY 10977', '845-425-9002', 'Malibu', 18, 'Pink', '12/04/2022', 90.00, 'New', '', '06/23/2023', 'Summer', 120.00
+union select 'Yonason Katz', '1470 E 26th Street Brooklyn NY 11223', '718-376-2658', 'Huffy', 20, 'Blue', '06/14/2023', 76.00, 'New', '', '08/03/2023', 'Summer', 130.00
+union select 'Bracha Smith', '25 North Rigaud Road Spring Valley NY 10977', '845-352-1099', 'Razor', 24, 'Slate', '05/18/2023', 167.00, 'New', '', '07/22/2023', 'Summer', 220.00
+union select 'Moshe Weiss', '25 Old Nyack Turnpike Monsey NY 10952', '845-356-9423', 'Kent', 24, 'Black', '12/13/2022', 103.00, 'Used', 'Minor Fixup', '08/16/2023', 'Summer', 195.00
+union select 'Yehuda Jacobs', '1650 Lexington Avenue Lakewood NJ 08701', '732-930-8054', 'Schwinn', 20, 'Blue',' 04/09/2022', 42.00, 'Used', 'Major Fixup', '07/20/2022', 'Summer', 98.00
