2b6ca55f2539ace6647c73319e8b5cd14b8c9560
Faiga-new <faigaeinhorn@gmail.com>
2025-07-23 12:55:12 -0400

    Update 3 Reports.sql

--- 3 Reports.sql
+++ 3 Reports.sql
@@ -1,9 +1,18 @@
 --1.
-select * from BikeShop where CustomerAddress like '%Spring Valley%' or CustomerAddress like '%Monsey%'
-select * from BikeShop where CustomerAddress not like '%Spring Valley%' and CustomerAddress not like '%Monsey%'
+select * 
+from BikeShop 
+where CustomerAddress like '%Spring Valley%' 
+or CustomerAddress like '%Monsey%'
+
+select * 
+from BikeShop 
+where CustomerAddress not like '%Spring Valley%' 
+and CustomerAddress not like '%Monsey%'
 
 --2.
-select BikesSold = count(*), SeasonSold from BikeShop group by SeasonSold
+select BikesSold = count(*), SeasonSold
+from BikeShop 
+group by SeasonSold
 
 --3.
 select AvgDaysInShop = avg(Datediff(day,datepurchased, datesold)), 
@@ -13,7 +22,11 @@ Profit = sum(soldprice) - sum(purchaseprice)
 from BikeShop
 
 --4.
-select ProfitForEachSale = soldprice - purchaseprice, customername, bikecompany, purchaseprice, soldprice, new from BikeShop
+select ProfitForEachSale = soldprice - purchaseprice, customername, bikecompany, purchaseprice, soldprice, new 
+from BikeShop
 
 --5.
-select top 1 BikesPerCompany = count(*), BikeCompany from BikeShop group by BikeCompany order by BikesPerCompany desc
+select top 1 BikesPerCompany = count(*), BikeCompany 
+from BikeShop 
+group by BikeCompany 
+order by BikesPerCompany desc
