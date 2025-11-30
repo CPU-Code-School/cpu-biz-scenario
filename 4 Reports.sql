e737b440fdb4b70e6818a190e27aa7cadd0c7838
yakovbardi <yakovbardi@gmail.com>
2025-11-30 17:50:25 -0500

    4 Reports Costume

---
+++ 4 Reports.sql
@@ -0,0 +1,18 @@
+USE CostumeStoreDB
+GO
+--I am going to need the following reports:
+--1. I need to know which costume is the most popular.
+SELECT COUNT(*),C.costume
+from Costume C
+GROUP BY C.costume
+--2. I need to know which size is the most polpular.
+SELECT COUNT(*),C.size
+from Costume C
+GROUP BY C.size
+--3. I need you to show me all of my customers in the following format: name: amountbought - costume customer bought (how much they paid)
+    --(YB I am supposing that "(how much they paid)" means -for each costume, (and not the total amount) since the "Amount Bought" is included in the report.)
+SELECT [format]=CONCAT(c.CustomerFirstName,' ',c.CustomerLastName,': ',c.amountbought,' - ',c.costume,' (',c.SalesPricePerCostume,')')
+from Costume C
+--4. I need to know the profit each sale.
+SELECT c.profit,C.CustomerFirstName,c.customerlastname,c.costume,c.Size,c.amountbought,c.SalesPricePerCostume,c.Discount,c.DateSold,c.PurchasePricePerCostume,c.PaidFullPrice,c.TotalPricePaid
+FROM Costume C
