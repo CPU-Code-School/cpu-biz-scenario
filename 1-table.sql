04e8cba74c2b682bd61cfe0fec851630b183f348
AvigayilSamowitz <143674019+AvigayilSamowitz@users.noreply.github.com>
2025-12-09 17:13:18 +0200

    Update 1-table.sql

--- 1-table.sql
+++ 1-table.sql
@@ -9,9 +9,8 @@ create table dbo.Soldier(
 	LastName varchar(25) not null
 		constraint c_Soldier_last_name_cannot_be_blank check(LastName <> ''),
 	SSN char(10) not null
-		constraint c_Soldier_SSN_cannot_be_blank check(SSN <> '')
 		constraint u_Soldier_SSN_must_be_unique unique(SSN),
-		constraint c_Soldier_SSN_must_be_8_digits_dash_1_digit check(SSN like '%-%'),
+		constraint c_Soldier_SSN_must_be_8_digits_dash_1_digit check(SSN like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9]'),
 	DateOfBirth date not null,
 	Residence varchar(25) not null
 		constraint c_Soldier_residence_cannot_be_blank check(Residence <> ''),
