cd11c66d175329c8e2db80f9c64fa7dafe835b3d
EstyWalter <estywalter4@gmail.com>
2026-01-19 16:03:05 -0500

    Update 2 table for recruiting educaters company.sql

--- recruitingeducaterdb/2 table for recruiting educaters company.sql
+++ recruitingeducaterdb/2 table for recruiting educaters company.sql
@@ -14,16 +14,16 @@ create table dbo.EducatersInfo(
     College varchar (30) not null
     constraint C_Recruiting_Educaters_College_not_blank check(college <> ''),
     TitleOfDegree varchar (35) not null
-    constraint C_Recruiting_Educaters_titel_of_degree_not_blank_and_starts_with_bachechelors_masters_or_PhD 
-    check(titleofdegree <> '' and (titleofdegree like 'MA %' or titleofdegree like 'BA %' or titleofdegree like 'PHD %')),
+    constraint C_Recruiting_Educaters_title_of_degree_starts_with_bachechelors_masters_or_PhD 
+    check(titleofdegree like 'MA %' or titleofdegree like 'BA %' or titleofdegree like 'PHD %'),
     Media varchar (35) not null
-    constraint C_Recruiting_Educaters_Media_not_blank_and_can_oly_enter_magazines_newspaper_social_media_sites_or_word_of_mouth
-    check(media <> '' and media in ('magazine', 'newspaper', 'social media site', 'word of mouth', 'social media')),
+    constraint C_Recruiting_Educaters_Media_must_be_eather_magazines_newspaper_social_media_sites_or_word_of_mouth
+    check (media in ('magazine', 'newspaper', 'social media site', 'word of mouth', 'social media')),
     DateContacted date not null
     constraint _Recruiting_Educaters_date_contacted_not_before_February_17_2017 check(datecontacted >= '2017-02-17'),
     SchoolPlaced varchar (30) null
-    constraint C_Recruiting_Educaters_school_placed_not_blank_and_located_in_Brooklyn_Manhattan_or_New_York 
-    check((schoolplaced <> ''and (schoolplaced like 'Brooklyn%' or schoolplaced like 'Manhattan%' or schoolplaced like 'New York%' or schoolplaced like 'New Jersey%'))),
+    constraint C_Recruiting_Educaters_school_placed_located_in_Brooklyn_Manhattan_or_New_York 
+    check((schoolplaced like 'Brooklyn%' or schoolplaced like 'Manhattan%' or schoolplaced like 'New York%' or schoolplaced like 'New Jersey%')),
     DateJobFound date null,
     constraint C_Recruiting_Educaters_eather_school_placed_and_date_job_found_are_both_null_or_both_not_null
     check((Datejobfound is null and schoolplaced is null) or (Datejobfound is not null and schoolplaced is not null)),
